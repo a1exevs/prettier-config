2c24e5012a804a0091a94082dc8249eff742bda1
Alexander Evstafiadi <58134475+a1exevs@users.noreply.github.com>
2025-06-04 14:24:14 +0300

    [Common] Switch from json to ts config (#6)

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'prettier';
+
+const config: Config = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  rangeStart: 0,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: false,
+};
+
+export default config;
